743c2835f20b556a84c2ec1ad629d5ee1322eceb
Stephen Roebuck <controll.face@gmail.com>
2013-09-11 19:13:10 -0400

    added seek and flee
    also renamed a few variables

--- GSP420_AI_CORE/GSP420_AI_CORE/Steering.h
+++ GSP420_AI_CORE/GSP420_AI_CORE/Steering.h
@@ -8,7 +8,7 @@ struct Kinematics
 {
 	float orientation;
 	float rotation;
-	float postion[3];//made for 3D, can be 2D
+	float position[3];//made for 3D, can be 2D
 	float velocity[3];//made for 3D, can be 2D
 }
 
@@ -16,25 +16,51 @@ class Steering
 {
 private:
 	Kinematics targetAgent;	//object we perform steering operations in accordance too
-	Kinematics currAgent;	//object we are steering
+	Kinematics currentAgent;	//object we are steering
 public:
 	void MatchVelocity();	//use to match velocity
 	void RotationAlign();	//use to face target like a turret
+	void Seek();
+	void Flee();
 }
 
 //match velocity to that of the target, made for 3D, can be 2D
 void Steering::MatchVelocity()
 {
-	currAgent.velocity[0] = targetAgent.velocity[0];
-	currAgent.velocity[1] = targetAgent.velocity[1];
-	currAgent.velocity[2] = targetAgent.velocity[2];
+	currentAgent.velocity[0] = targetAgent.velocity[0];
+	currentAgent.velocity[1] = targetAgent.velocity[1];
+	currentAgent.velocity[2] = targetAgent.velocity[2];
 }
 
 //turn towards target, *****INCOMPLETE*****
 void Steering::RotationAlign()
 {
-	currAgent.rotation = targetAgent.orientation - currAgent.orientation;
+	currentAgent.rotation = targetAgent.orientation - currentAgent.orientation;
 	//now need to put the rotation value in the range of -PI and PI
 	
+}
+
+void Steering::Seek()
+{
+
+	bool xDirection = currentAgent.position[0] > targetAgent.position[0];
+	bool yDirection = currentAgent.position[1] > targetAgent.position[1];
+	bool zDirection = currentAgent.position[2] > targetAgent.position[2];
+
+	((xDirection) ? currentAgent.position[0]-- : currentAgent.position[0]++); 
+	((yDirection) ? currentAgent.position[1]-- : currentAgent.position[1]++);
+	((zDirection) ? currentAgent.position[2]-- : currentAgent.position[2]++);
+}
+
+void Steering::Flee()
+{
+
+	bool xDirection = currentAgent.position[0] > targetAgent.position[0];
+	bool yDirection = currentAgent.position[1] > targetAgent.position[1];
+	bool zDirection = currentAgent.position[2] > targetAgent.position[2];
+
+	((xDirection) ? currentAgent.position[0]++ : currentAgent.position[0]--); 
+	((yDirection) ? currentAgent.position[1]++ : currentAgent.position[1]--);
+	((zDirection) ? currentAgent.position[2]++ : currentAgent.position[2]--);
 }
 
