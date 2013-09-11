9ff17ea134c5f600e8e650c398f71cf5399b4675
Joshua Murrill <murrills@hotmail.com>
2013-09-10 19:10:23 -0700

    added comments

--- GSP420_AI_CORE/GSP420_AI_CORE/Steering.h
+++ GSP420_AI_CORE/GSP420_AI_CORE/Steering.h
@@ -1,25 +1,28 @@
+//used for rotational calculations, mainly for 3D
 #define PI ((float)3.141592654f)
 #define ToRadian(degree) ((degree) * (PI / 180.0f))
 #define ToDegree(radian) ((radian) * (180.0f / PI))
 
+//an example of the data structure we need to perform steering
 struct Kinematics
 {
 	float orientation;
 	float rotation;
-	float postion[3];
-	float velocity[3];
+	float postion[3];//made for 3D, can be 2D
+	float velocity[3];//made for 3D, can be 2D
 }
 
 class Steering
 {
 private:
-	Kinematics targetAgent;
-	Kinematics currAgent;
+	Kinematics targetAgent;	//object we perform steering operations in accordance too
+	Kinematics currAgent;	//object we are steering
 public:
-	void MatchVelocity();
-	void RotationAlign();
+	void MatchVelocity();	//use to match velocity
+	void RotationAlign();	//use to face target like a turret
 }
 
+//match velocity to that of the target, made for 3D, can be 2D
 void Steering::MatchVelocity()
 {
 	currAgent.velocity[0] = targetAgent.velocity[0];
@@ -27,9 +30,11 @@ void Steering::MatchVelocity()
 	currAgent.velocity[2] = targetAgent.velocity[2];
 }
 
+//turn towards target, *****INCOMPLETE*****
 void Steering::RotationAlign()
 {
 	currAgent.rotation = targetAgent.orientation - currAgent.orientation;
+	//now need to put the rotation value in the range of -PI and PI
 	
 }
 
